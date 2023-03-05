3d1dc32f097370b5a5d4a81f1a5422cd2e1e5fac
Bisabil Nizam] <nbisabil@gmail.com>
2023-03-05 16:31:20 +0800

    bout to go over props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 
 import "./App.css";
+import Employee from "./component/Employee";
 
 function App() {
-
+    const showEmployee = false
+    return (
+        <div className="App">
+            {showEmployee ?  (
+                <>
+                    <Employee />
+                    <Employee />
+                    <Employee />
+                    <Employee />
+                </>
+            ) : (
+                <p>This is not employee</p>
+            )}
+        </div>
+)
+     
 }
 
 export default App;
---
+++ src/component/Employee.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Employee() {
+  return (
+    <div>
+      <h3>Here is the emplyee</h3>
+    </div>
+  )
+}
