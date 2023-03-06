4d91697bd64500bf7ec9a1512b0750d3b9a7984d
Bisabil Nizam] <nbisabil@gmail.com>
2023-03-06 11:18:53 +0800

    initiate tailwind

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-
 import "./App.css";
 import Employee from "./component/Employee";
+import { useState } from "react";
 
 function App() {
-    const showEmployee = false
-    return (
-        <div className="App">
-            {showEmployee ?  (
-                <>
-                    <Employee />
-                    <Employee />
-                    <Employee />
-                    <Employee />
-                </>
-            ) : (
-                <p>This is not employee</p>
-            )}
-        </div>
-)
-     
+    const [ role, setRole ] = useState("dev");
+    const showEmployee = true;
+    // ------------------------------//
+  return (
+    <div className="App">
+      {showEmployee ? (
+        <>
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+          />
+          <Employee name="Bisabil" role="intern" />
+          <Employee name="Nasha" role="Isteri" />
+          <Employee name="Elman" role={role} />
+        </>
+      ) : (
+        <p>This is not employee</p>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/component/Employee.js
+++ src/component/Employee.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function Employee() {
+export default function Employee(props) {
   return (
     <div>
-      <h3>Here is the emplyee</h3>
+      <h3>Here is the {props.name}</h3>
+      <p>{props.role ? props.role : 'There are not available'}</p>
     </div>
   )
 }
